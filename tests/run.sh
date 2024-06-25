901ae9df763c0dc092c55bee8aedadcfb3073b2e
Hugo Mercier <hugo.mercier@iot.bzh>
2024-06-25 14:08:41 +0200

    Fix tests/run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-TEST_BINDING_PATH=${SCRIPT_DIR}/../build python ${SCRIPT_DIR}/tests.py --tap
+LD_LIBRARY_PATH=${SCRIPT_DIR}/../build python ${SCRIPT_DIR}/tests.py
