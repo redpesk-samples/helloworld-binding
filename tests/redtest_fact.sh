611e4b0e5190d3f813fdfcc8749e13c792e5e17d
Iheb Ben Gara Ali <iheb.bengaraali@iot.bzh>
2025-06-03 16:47:04 +0200

    Add template rp-cli ci

--- tests/redtest_fact.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e  # Exit immediately if a command exits with a non-zero status
-
-RP_REMOTE_FACTORY_PROJECT=$1
-
-# Run the redtest for the hello-world-binding application
-rp-cli applications test helloworld-binding --project "$RP_REMOTE_FACTORY_PROJECT"
